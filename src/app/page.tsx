6bf0bf5711e59bf13ca78689255362dec40dd5c4
AdiDance <adriandance88@gmail.com>
2024-10-09 14:31:05 +0300

    initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+export default function Home() {
+  // What is CORS?
+  // Let's say we can't call https://jsonplaceholder.typicode.com/photos because of CORS
+  // create API which gets the photos from https://jsonplaceholder.typicode.com/photos by
+  //
+  // create 3 sections in the homepage for each album from https://jsonplaceholder.typicode.com/photos
+  // each section should be linked to a dedicated page
+  // create a dynamic page (/album/albumId) which displays the images from the corresponding album
+
+  return <div>display the sections here</div>;
+}
